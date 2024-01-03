0f1ca1bc34639523b90bf4954ed6e0c7d8f404f0
Vitaliy Orazov <viordash@mail.ru>
2024-01-03 16:58:30 +0300

    fix uart test Debug_VeryDifficult_Case

--- UTestsForDebug_UART/UTestsForDebug_UART_Tests/communication_tests.cpp
+++ UTestsForDebug_UART/UTestsForDebug_UART_Tests/communication_tests.cpp
@@ -1,4 +1,7 @@
 #include <CppUTest/CommandLineTestRunner.h>
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
 #include <stdio.h>
 #include "main.h"
 
@@ -10,10 +13,11 @@
 TEST_GROUP(CommunicationTestGroup) {
 	TEST_SETUP() {
 		SystemClock_Config();
-		UART_Init();
 	}	
 	
-	TEST_TEARDOWN() {}	
+	TEST_TEARDOWN() {
+		mock().clear();
+	}	
 }
 ;
 
@@ -33,12 +37,48 @@ void BSP_LED_Toggle(Led_TypeDef Led) {
 }
 
 TEST(CommunicationTestGroup, Debug_VeryDifficult_Case) {	
-	uint8_t payload[6] = { 0, 1, 2, 3, 4, 5 };
+	TCommandId id;
 	
-	UART_SendCommand(TCommandId::cmd_VeryDifficult, 2, payload);
+	id = TCommandId::cmd_VeryDifficult;
+	
+	mock()
+		.expectOneCall("UART_HandleReceivingCommands")
+		.withOutputParameterReturning("id", &id, sizeof(id))
+		.andReturnValue(true);
+	
+	mock()
+		.expectOneCall("UART_SendCommand")
+		.withParameter("id", TCommandId::cmd_Start)
+		.withParameter("status", 0); 
+	
 	Comm_ProcessMessages();
 	
 	CHECK_EQUAL_TEXT(LED6, led_On, "Perform_Command_VeryDifficult was not called");
 	CHECK_EQUAL_TEXT(LED5, led_Off, "Perform_Command_VeryDifficult was not called");
+	
+	mock().checkExpectations();
 }
 
+/* mocking uart module*/
+
+void UART_Init() {
+	mock().actualCall("UART_Init");
+}
+
+void UART_SendCommand(TCommandId id, uint8_t status, uint8_t *payload) {
+	(void)payload;
+	
+	mock().actualCall("UART_SendCommand")
+		.withIntParameter("id", id)
+		.withUnsignedIntParameter("status", status); 
+}
+
+bool UART_HandleReceivingCommands(TCommandId *id, uint8_t *status, uint8_t *payload, size_t payload_size) {	
+	(void)status;
+	(void)payload;
+	(void)payload_size;
+	
+	return mock().actualCall("UART_HandleReceivingCommands")
+		.withOutputParameter("id", id)
+		.returnBoolValue(); 
+}
